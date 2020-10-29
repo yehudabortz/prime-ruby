dd779a51c641c81f671e526cd617b9a0529b6f68
Yehuda Bortz <yehudabortz@icloud.com>
2020-10-29 13:44:13 -0500

    commit tree 7704b30ee24c17b1e4b6d48fa307e092870f8f7a

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+require 'pry'
+
+def prime?(integer)
+    if  integer < 0 || integer == 0 || integer == 1
+    false
+    else
+        (2..integer - 1).to_a.all? do |num|
+            integer % num != 0 
+        end
+    end
+end
+
